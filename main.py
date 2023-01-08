9d8ccdabeea9481d4474b60315dfb0f8df3bd395
David <dav.gruber@htlstp.at>
2023-01-08 01:20:06 +0100

    initial-structure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import discord
+
 import DiscordHandler.DiscordHandler as dh
 import Service.ServiceRunner as sr
 
 if __name__ == '__main__':
-    bot = dh.DiscordHandler()
+    bot = dh.DiscordHandler(intents=discord.Intents.default())
     bot.run(sr.get_bot_token())
